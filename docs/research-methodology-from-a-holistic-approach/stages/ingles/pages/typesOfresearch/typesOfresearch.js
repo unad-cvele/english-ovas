4614a852c00dd6074312ab56bb4dcf9eb52e1013
Edison Johan Bernal <edisonjbernal@gmail.com>
2022-04-26 11:58:56 -0500

    ajuste video REsearch

--- docs/research-methodology-from-a-holistic-approach/stages/ingles/pages/typesOfresearch/typesOfresearch.js
+++ docs/research-methodology-from-a-holistic-approach/stages/ingles/pages/typesOfresearch/typesOfresearch.js
@@ -10,7 +10,7 @@ class inglesClass_typesOfresearch extends generalPage{
   content(){
       return `
       <div class="header-h2"><h2>Types of research</h2></div>
-      <video class="responsive" src="./images/Video3-F.mp4" controls="" controlslist="nodownload"></video>
+      <iframe class="iframe-video-principal" src="https://www.youtube.com/embed/3bptYhZyK1Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div class="text-center">
         <button class="btn-general btn-back" onclick="stages_ingles.menu.back()">Back</button>
         <button class="btn-general btn-next" onclick="stages_ingles.menu.next()">Next</button>
